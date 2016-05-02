78b04686be3121659412ce7ea3d6a31886c8935b
AnitaYip <anita.yip@realpage.com>
2016-05-02 10:07:47 -0400

    functions with pass by reference

--- functions.php
+++ functions.php
@@ -1,25 +1,28 @@
 <?php
 
-function add($var1, $var2){
+function add(&$var1, &$var2){
 	return $var1+$var2;
 }
 
-function subtract($var1, $var2){
+function subtract(&$var1, &$var2){
 	return $var1-$var2;
 }
-function multiply($var1, $var2){
+function multiply(&$var1, &$var2){
 	return $var1*$var2;
 }
 
-function devide($var1, $var2){
+function devide(&$var1, &$var2){
 	return $var1/$var2;
 }
 
-echo add(1,2) . "\n";
+$var1 = 1;
+$var2 = 2;
 
-echo subtract(1,2) . "\n";
+echo add($var1, $var2) . "\n";
 
-echo multiply(1,2) . "\n";
+echo subtract($var1, $var2) . "\n";
 
-echo devide(1,2) . "\n";
+echo multiply($var1, $var2) . "\n";
+
+echo devide($var1, $var2) . "\n";
 ?>
