c1187753e39ac1c361a08e31f675e322ec25a3d2
AnitaYip <anita.yip@realpage.com>
2016-05-02 10:40:12 -0400

    variable arguments

--- math.php
+++ math.php
@@ -2,18 +2,34 @@
 
 class math{
 
-	public function add(&$var1, &$var2){
-		return $var1+$var2;
+	public function add(int $var1, int $var2, ...$more){
+		$res = $var1+$var2;
+		foreach ($more as $value) {
+			$res += $value;
+		}
+		return $res;
 	}
 
-	public function subtract(&$var1, &$var2){
-		return $var1-$var2;
+	public function subtract(int $var1, int $var2, ...$more){
+		$res = $var1-$var2;
+		foreach ($more as $value) {
+			$res -= $value;
+		}
+		return $res;
 	}
-	public function multiply(&$var1, &$var2){
-		return $var1*$var2;
+	public function multiply(int $var1, int $var2, ...$more){
+		$res = $var1*$var2;
+		foreach ($more as $value) {
+			$res *= $value;
+		}
+		return $res;
 	}
 
-	public function divide(&$var1, &$var2){
-		return $var1/$var2;
+	public function divide(int $var1, int $var2, ...$more){
+		$res = $var1/$var2;
+		foreach ($more as $value) {
+			$res /= $value;
+		}
+		return $res;
 	}
 }
