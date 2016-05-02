9c7e89e810d216dd089568e03265524c3a830fbd
AnitaYip <anita.yip@realpage.com>
2016-05-02 10:14:05 -0400

    print result using all the math functions

--- functions.php
+++ functions.php
@@ -11,18 +11,13 @@ function multiply(&$var1, &$var2){
 	return $var1*$var2;
 }
 
-function devide(&$var1, &$var2){
+function divide(&$var1, &$var2){
 	return $var1/$var2;
 }
 
 $var1 = 1;
 $var2 = 2;
 
-echo add($var1, $var2) . "\n";
+echo "((($var1 + $var2) - $var1 ) * $var2) / $var2 = " . divide(multiply(subtract(add($var1,$var2), $var1), $var2), $var2) . "\n";
 
-echo subtract($var1, $var2) . "\n";
-
-echo multiply($var1, $var2) . "\n";
-
-echo devide($var1, $var2) . "\n";
 ?>
