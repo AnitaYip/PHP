1dbb68ac4c0832b46db7dbccac6a4c040a508833
AnitaYip <anita.yip@realpage.com>
2016-05-02 09:38:54 -0400

    functions with pass by value

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function add($var1, $var2){
+	return $var1+$var2;
+}
+
+function subtract($var1, $var2){
+	return $var1-$var2;
+}
+function multiply($var1, $var2){
+	return $var1*$var2;
+}
+
+function devide($var1, $var2){
+	return $var1/$var2;
+}
+
+echo add(1,2) . "\n";
+
+echo subtract(1,2) . "\n";
+
+echo multiply(1,2) . "\n";
+
+echo devide(1,2) . "\n";
+?>
