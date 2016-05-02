8a8ac5e46410b44c0ca8cbd14c87a7b466ec9796
AnitaYip <anita.yip@realpage.com>
2016-05-02 10:31:10 -0400

    add comparison function

--- functions.php
+++ functions.php
@@ -15,9 +15,20 @@ function divide(&$var1, &$var2){
 	return $var1/$var2;
 }
 
+function comparison($var1, $var2, $compareType = false) {
+	$result = ($compareType ? $var1 === $var2 : $var1 == $var2);
+	echo "The parameters '$var1' and '$var2' are " . ($result ? "equal" : "not equal") . ($compareType ? " by datatype " : "") . "\n";
+}
+
 $var1 = 1;
 $var2 = 2;
 
 echo "((($var1 + $var2) - $var1 ) * $var2) / $var2 = " . divide(multiply(subtract(add($var1,$var2), $var1), $var2), $var2) . "\n";
+
+echo comparison(4,"4") . "\n";
+echo comparison(5,"5",true) . "\n";
+echo comparison(4,4.0) . "\n";
+echo comparison(5,5.0,true) . "\n";
+
 
 ?>
