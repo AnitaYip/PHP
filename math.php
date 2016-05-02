dc11a21ac5a160e44a23d25f8fb3fa60d5fefb9e
AnitaYip <anita.yip@realpage.com>
2016-05-02 10:33:59 -0400

    adding math class

---
+++ math.php
@@ -0,0 +1,19 @@
+<?php
+
+class math{
+
+	public function add(&$var1, &$var2){
+		return $var1+$var2;
+	}
+
+	public function subtract(&$var1, &$var2){
+		return $var1-$var2;
+	}
+	public function multiply(&$var1, &$var2){
+		return $var1*$var2;
+	}
+
+	public function divide(&$var1, &$var2){
+		return $var1/$var2;
+	}
+}
